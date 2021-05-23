eb37c6ad283eb12f2cf91cf1f671d3c075bd91e9
Vineet Verma <vineet@styldod.com>
2021-05-23 14:30:32 +0530

    Formatted mssge plus now sending mssge only when more that 1 vaccine is availble bcoz unable to book those

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const getCenters = async (districtId, date) => {
     try {
-        const resp = await axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${districtId}&date=${date}`, {
+        const resp = await axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=${districtId}&date=${date}`, {
             headers: {
                 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
                 'Accept-Language': 'en_US',
@@ -21,17 +21,19 @@ const getCenters = async (districtId, date) => {
 
 async function getFilteredCenters(age) {
     const centers = await getCenters('313', moment().format('DD-MM-YYYY'))
-    const filteredCenters = centers.filter(center => {
+    const formattedMessgae = []
+    centers.map(center => {
         const sessions = center.sessions
         for (const session of sessions) {
-            if (session.available_capacity > 0 && session.min_age_limit === age && session.date >= moment().format('DD-MM-YYYY')) return true
+            if (session.available_capacity > 1 && session.min_age_limit === age && session.date >= moment().format('DD-MM-YYYY')) {
+                formattedMessgae.push("<b>Name:</b> " + center.name + '\n' + "<b>Address:</b> " + center.address + '\n' + "<b>Available Capacity:</b> " + session.available_capacity + '\n' + "<b>Vaccine Name:</b> " + session.vaccine + '\n' + "<b>Date:</b> " + session.date + '\n\n')
+            }
         }
     })
 
-    // console.log(filteredCenters)
-    return filteredCenters
+    // console.log(formattedMessgae)
+    return formattedMessgae
 }
-
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TOKEN
@@ -51,14 +53,7 @@ const telegrambot = (message) => {
 }
 
 async function sendMessage() {
-    const filteredCenters = await getFilteredCenters(18)
-    let formattedMessgae = []
-    if (filteredCenters.length > 0) {
-        formattedMessgae = filteredCenters.map(center => {
-            return "<b>Name:</b> " + center.name + '\n' + "<b>Address:</b> " + center.address + '\n\n'
-        })
-    }
-
+    const formattedMessgae = await getFilteredCenters(18)
     if (formattedMessgae.length > 0) {
         telegrambot(`<b>Vaccine Available. Schedule ASAP</b>\n\n${formattedMessgae.join('')}`)
     }
